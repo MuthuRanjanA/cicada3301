1736a787bd83651d8f093f92f2bf2448f08c99e8
MuthuRanjanA <139042338+MuthuRanjanA@users.noreply.github.com>
2024-04-15 19:28:33 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -19,7 +19,7 @@ s.write("___")
 s.write("""_IF YOU WANT THE ANSWER FOR THE EVENT , ANSWER THE QUESTION_""")
 c1,c2=s.columns(2)
 with c1:
-    a1=s.number_input("what is the value bitcoin in 1st jan 2024 with 2 floating point?")
+    a1=s.number_input("what is the price of bitcoin in dollers on 1st jan 2024 with 2 floating point?")
 with c2:
     a2=s.number_input("what is the value of phi or golden ratio with 2 floating point?")
 if (a1==43835.62) and (a2==1.62):
